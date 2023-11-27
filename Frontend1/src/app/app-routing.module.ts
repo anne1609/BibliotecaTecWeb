7323e9e83c5750a66d9d530012b93dbae05dda7a
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2023-11-27 17:02:47 -0400

    adicion rutas

--- Frontend1/src/app/app-routing.module.ts
+++ Frontend1/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrarLibroComponent } from './paginas/registrar-libro/registrar-libro.component';
+import { LibrosComponent } from './paginas/libros/libros.component';
 
 
 const routes: Routes = [
   { path: 'registrar-libro', component: RegistrarLibroComponent },
+  { path: 'libros', component: LibrosComponent },
+  { path: '', redirectTo: '/libros', pathMatch: 'full' }
 ];
 
 @NgModule({
