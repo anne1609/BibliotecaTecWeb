5d52dc6d408bb0af247a804b4a04f740bc951fce
Anaelise Huaniri <aanesita@gmail.com>
2023-12-04 19:10:55 -0400

    Validaciones agregadas al form de inicio de sesion

--- Frontend1/src/app/paginas/inicio-sesion/inicio-sesion.component.ts
+++ Frontend1/src/app/paginas/inicio-sesion/inicio-sesion.component.ts
@@ -1,14 +1,30 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio-sesion',
   templateUrl: './inicio-sesion.component.html',
-  styleUrl: './inicio-sesion.component.css',
+  styleUrls: ['./inicio-sesion.component.css'],
   encapsulation: ViewEncapsulation.None,
-  
 })
 export class InicioSesionComponent {
   hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', Validators.required);
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Debes ingresar un valor';
+    }
+
+    return this.email.hasError('email') ? 'No es un correo electrónico válido' : '';
+  }
+
+  getPasswordErrorMessage() {
+    if (this.password.hasError('required')) {
+      return 'Debes ingresar una contraseña';
+    }
+
+    return '';
+  }
 }
-
