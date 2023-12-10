0ff4c12df45d4af79562293c7135b9dd35484d30
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2023-12-10 10:30:31 -0400

    calcula la cotizacion

--- Frontend1/src/app/elementos/pop-up-prestamo/pop-up-prestamo.component.ts
+++ Frontend1/src/app/elementos/pop-up-prestamo/pop-up-prestamo.component.ts
@@ -1,34 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pop-up-prestamo',
   templateUrl: './pop-up-prestamo.component.html',
-  styleUrl: './pop-up-prestamo.component.css'
+  styleUrls: ['./pop-up-prestamo.component.css']
 })
-export class PopUpPrestamoComponent {
+export class PopUpPrestamoComponent implements OnDestroy, OnInit {
   startDate = new FormControl(new Date());
   endDate = new FormControl(new Date());
+  diasDiferencia: number = 0;
+  costoPorDia: number = 5;
+  costoTotal: number = 0;
+  private subscriptions: Subscription[] = [];
+  minDate: Date = new Date(); 
 
-  constructor(public dialogRef: MatDialogRef<PopUpPrestamoComponent >) {}
+  constructor(public dialogRef: MatDialogRef<PopUpPrestamoComponent>) {
+    this.subscribeToFormChanges();
+  }
+  ngOnInit(): void {
+    this.calcularCotizacion();
+    this.minDate = new Date();
+  }
+
+  private subscribeToFormChanges(): void {
+    this.subscriptions.push(
+      this.startDate.valueChanges.subscribe(() => {
+        this.calcularCotizacion();
+      }),
+      this.endDate.valueChanges.subscribe(() => {
+        this.calcularCotizacion();
+      })
+    );
+  }
+
+  calcularCotizacion(): void {
+  const startDateValue = this.startDate.value;
+  const endDateValue = this.endDate.value;
+
+  if (startDateValue && endDateValue) {
+    const start = new Date(startDateValue instanceof Date ? startDateValue : new Date(startDateValue));
+    const end = new Date(endDateValue instanceof Date ? endDateValue : new Date(endDateValue));
+
+    const diferenciaMs = Math.abs(end.getTime() - start.getTime());
+    this.diasDiferencia = Math.ceil(diferenciaMs / (1000 * 60 * 60 * 24));
+
+    this.diasDiferencia += 1;
+    
+
+    this.costoTotal = this.diasDiferencia * this.costoPorDia;
+  }
+}
+
 
   cerrar(): void {
     this.dialogRef.close();
   }
 
   confirmar(): void {
-
     if (this.startDate.value && this.endDate.value) {
       this.dialogRef.close({
         startDate: this.startDate.value.toISOString(),
-        endDate: this.endDate.value.toISOString()
+        endDate: this.endDate.value.toISOString(),
+        diasDiferencia: this.diasDiferencia,
+        costoTotal: this.costoTotal
       });
     } else {
-      //solo en caso q sea null
+      // Manejar el caso cuando startDate o endDate son null
     }
   }
 
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
+
 
 
